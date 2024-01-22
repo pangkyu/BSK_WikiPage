94b538afcae6acf78bfdaa80385795cc3cf7d8fe
pangkyu <baesense@naver.com>
2024-01-22 22:11:59 +0900

    Feat : 라우팅 기능

--- src/components/ContentBox.tsx
+++ src/components/ContentBox.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface ContentBoxProps {
   title: string;
+  id?: number;
 }
 
-const ContentBox: React.FC<ContentBoxProps> = ({ title }) => {
+const ContentBox: React.FC<ContentBoxProps> = ({ title, id }) => {
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    navigate(`/detail/${id}`);
+  };
   return (
-    <section className="flex flex-row w-[85vw] h-10 my-5 mx-auto cursor-pointer">
+    <section
+      className="flex flex-row w-[85vw] h-10 my-5 mx-auto cursor-pointer"
+      onClick={handleOnClick}
+    >
       <div className="flex w-12">
         <input type="checkbox" />
       </div>
